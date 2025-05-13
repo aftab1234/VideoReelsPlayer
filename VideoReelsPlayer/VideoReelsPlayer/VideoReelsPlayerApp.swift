522afcb1582b0d5bd94bedda159730fe7f90406a
Gourav <gourav@tepia.co>
2025-05-13 07:52:55 +0530

    Added initial project setup

---
+++ VideoReelsPlayer/VideoReelsPlayer/VideoReelsPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoReelsPlayerApp.swift
+//  VideoReelsPlayer
+//
+//  Created by MOHAMMAD AFTAB SABIR on 13/05/25.
+//
+
+import SwiftUI
+
+@main
+struct VideoReelsPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
